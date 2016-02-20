19b5bd3351540a564fdc699986c4f1b3adae2d79
Shana Azria <shanaazria2016@u.northwestern.edu>
2016-02-20 06:34:23 -0600

    added sign up + login

---
+++ ProgrammingGame/LoginViewController.h
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.h
+//  ProgrammingGame
+//
+//  Created by Shana Azria Dev on 2/20/16.
+//  Copyright © 2016 Shana Azria Dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+@property (weak, nonatomic) IBOutlet UIButton *confirmButton;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *errorMessageLabel;
+@property (weak, nonatomic) IBOutlet UIButton *loginButton;
+
+
+- (IBAction)pressedLogin:(id)sender;
+
+- (IBAction)didConfirm:(id)sender;
+@end
---
+++ ProgrammingGame/LoginViewController.m
@@ -0,0 +1,120 @@
+//
+//  LoginViewController.m
+//  ProgrammingGame
+//
+//  Created by Shana Azria Dev on 2/20/16.
+//  Copyright © 2016 Shana Azria Dev. All rights reserved.
+//
+
+#import "LoginViewController.h"
+#import <Firebase/Firebase.h>
+
+@interface LoginViewController () <UITextFieldDelegate>
+
+@end
+
+@implementation LoginViewController
+Firebase *myRootRef;
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    myRootRef = [[Firebase alloc] initWithUrl:@"https://programminggame.firebaseio.com/"];
+    
+    self.confirmButton.clipsToBounds = YES;
+    self.confirmButton.layer.cornerRadius = 5;
+    
+    //text fields
+    self.usernameTextField.delegate = self;
+    self.passwordTextField.delegate = self;
+    self.passwordTextField.secureTextEntry = YES;
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    [self.usernameTextField resignFirstResponder];
+    [self.passwordTextField resignFirstResponder];
+    
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField*)textField
+{
+    
+    if ([textField isEqual:self.usernameTextField]) {
+        [self.passwordTextField becomeFirstResponder];
+    } else {
+        [textField resignFirstResponder];
+    }
+    return NO; // We do not want UITextField to insert line-breaks.
+}
+
+- (IBAction)pressedLogin:(id)sender {
+    self.errorMessageLabel.hidden = YES;
+    if ([self.titleLabel.text isEqualToString:@"Sign Up"]) {
+        self.titleLabel.text = @"Login";
+        [self.loginButton setEnabled:NO];
+        [self.loginButton setTitle:@"Cancel" forState:UIControlStateNormal];
+    } else {
+        self.titleLabel.text = @"Sign Up";
+        [self.loginButton setEnabled:NO];
+        [self.loginButton setTitle:@"Log in" forState:UIControlStateNormal];
+    }
+    [self.loginButton setEnabled:YES];
+    
+}
+
+- (IBAction)didConfirm:(id)sender {
+    [self.confirmButton setEnabled:NO];
+    if ([self.passwordTextField.text isEqualToString:@""] || [self.usernameTextField.text isEqualToString:@""]) {
+        self.errorMessageLabel.text = @"Please enter a username/password";
+        self.errorMessageLabel.hidden = NO;
+        [self.confirmButton setEnabled:YES];
+    } else if ([self.titleLabel.text isEqual:@"Login"]) {
+        [myRootRef authUser:self.usernameTextField.text password:self.passwordTextField.text
+  withCompletionBlock:^(NSError *error, FAuthData *authData) {
+          if (error) {
+              // There was an error logging in to this account
+              self.errorMessageLabel.text = @"Login error";
+              self.errorMessageLabel.hidden = NO;
+          } else {
+              // We are now logged in
+          }
+        }];
+        [self.confirmButton setEnabled:YES];
+        self.passwordTextField.text = @"";
+        self.usernameTextField.text = @"";
+    } else {
+        [myRootRef createUser:self.usernameTextField.text password:self.passwordTextField.text
+     withValueCompletionBlock:^(NSError *error, NSDictionary *result) {
+             if (error) {
+                 // There was an error creating the account
+                 NSLog(@"Error - %@", error);
+                 self.errorMessageLabel.text = @"Sign up error";
+                 self.errorMessageLabel.hidden = NO;
+             } else {
+                 self.errorMessageLabel.hidden = YES;
+                 NSString *uid = [result objectForKey:@"uid"];
+                 NSLog(@"Successfully created user account with uid: %@", uid);
+             }
+        }];
+        [self.confirmButton setEnabled:YES];
+        self.passwordTextField.text = @"";
+        self.usernameTextField.text = @"";
+    }
+    
+}
+@end
